3a435caf5635f2a70bf94aa7f88c35aca9511c1b
Saurabh Shinde <108724393+Surbh77@users.noreply.github.com>
2022-10-01 17:55:09 +0530

    Changes done requested from neogcamp team

--- Fun_triangles/quiz.js
+++ Fun_triangles/quiz.js
@@ -3,7 +3,7 @@ const submitAnsBtn=document.querySelector('#submit-answer-btn');
 const output=document.querySelector('#output');
 
 
-const correctAnswers=["90°","Right angle"]
+const correctAnswers=["90°","Right angle","Equilateral","Three uneven sides","Two equal sides","Acute","a+b+c","0","square of the ratio of their corresponding sides","Isosceles"]
 
 function calculateScore(){
     let score=0;
